2a7e44f063bfc6a06fd5ed7ee044dff0a262c6dc
Merryl DMello <rolan86@gmail.com>
2026-03-22 15:00:06 +0100

    feat(frontend): wire ToastContainer and provide toast injection

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,20 @@
 <script setup>
+import { provide, ref } from 'vue'
 import AppShell from '@/components/AppShell.vue'
+import ToastContainer from '@/components/ToastContainer.vue'
+
+const toastRef = ref(null)
+
+function toast(message, type = 'info', duration) {
+  toastRef.value?.addToast(message, type, duration)
+}
+
+provide('toast', toast)
 </script>
 
 <template>
   <AppShell>
     <RouterView />
   </AppShell>
+  <ToastContainer ref="toastRef" />
 </template>
